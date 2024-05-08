2ece16e33063c48c0c2e41c36bb8a0e65eae87bb
alexander <alex@polymathrobotics.com>
2024-05-08 09:59:35 -0700

    removed debug mssage

--- src/radar_layer.cpp
+++ src/radar_layer.cpp
@@ -241,7 +241,7 @@ void RadarLayer::updateBounds(
           uint8_t current_cost = costmap_[index];
           // RCLCPP_INFO(logger_, "probability: %f", probability);
           // RCLCPP_INFO(logger_, "sqrt_2_pi_det_covariance: %f", sqrt_2_pi_det_covariance);
-          RCLCPP_INFO(logger_, "cost: %i", current_cost);
+          // RCLCPP_INFO(logger_, "cost: %i", current_cost);
           costmap_[index] = std::max(current_cost, uint8_t(LETHAL_OBSTACLE * probability * sqrt_2_pi_det_covariance));
           
           //costmap_[index] = LETHAL_OBSTACLE;
