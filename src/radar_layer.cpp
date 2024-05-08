f8928438f5dcdccbec471e3589c260fd1c483e25
alexander <alex@polymathrobotics.com>
2024-05-08 13:52:57 -0700

    returing projected covariance

--- src/radar_layer.cpp
+++ src/radar_layer.cpp
@@ -564,8 +564,8 @@ Eigen::VectorXd RadarLayer::projectMean(
 
   position_projected = position + time_steps * sample_time * velocity;
 
-  //return position_projected;
-  return position;
+  return position_projected;
+  //return position;
 }
 
 Eigen::MatrixXd RadarLayer::projectCovariance(
@@ -586,8 +586,8 @@ Eigen::MatrixXd RadarLayer::projectCovariance(
   position_covariance_projected = position_covariance +
     pow(time_steps * sample_time, 2) * velocity_covariance;
 
-  //return position_covariance_projected;
-  return position_covariance;
+  return position_covariance_projected;
+  //return position_covariance;
 }
 
 } // namespace radar_layer
