9e314e5f14aff0cddbfe2eb7845069791103a038
alexander <alex@polymathrobotics.com>
2024-05-09 11:01:22 -0700

    only evaluate transform if number of objects is greater than 0

--- src/radar_layer.cpp
+++ src/radar_layer.cpp
@@ -192,24 +192,27 @@ void RadarLayer::updateBounds(
     int number_of_objects =
       obstacle_array->obstacles.size();
 
-    // RCLCPP_INFO(logger_, "target_frame: %s", global_frame_.c_str());
-    // RCLCPP_INFO(logger_, "source_frame: %s", obstacle_array->header.frame_id.c_str());
-    
-    // geometry_msgs::msg::TransformStamped radar_to_global_transform = tf_->lookupTransform(global_frame_, obstacle_array->header.frame_id, clock_->now(), rclcpp::Duration::from_seconds(2.0));
-    
-    // tf2::Quaternion q(
-    //   radar_to_global_transform.transform.rotation.x,
-    //   radar_to_global_transform.transform.rotation.y,
-    //   radar_to_global_transform.transform.rotation.z,
-    //   radar_to_global_transform.transform.rotation.w);
-  
-    // tf2::Matrix3x3 m(q);
-    // double roll, pitch, yaw;
-    // m.getRPY(roll, pitch, yaw);
 
-    // RCLCPP_INFO(logger_, "roll: %f", roll);
-    // RCLCPP_INFO(logger_, "pitch: %f", pitch);
-    // RCLCPP_INFO(logger_, "yaw: %f", yaw);
+    if(number_of_objects > 0){
+      RCLCPP_INFO(logger_, "target_frame: %s", global_frame_.c_str());
+      RCLCPP_INFO(logger_, "source_frame: %s", obstacle_array->header.frame_id.c_str());
+      
+      geometry_msgs::msg::TransformStamped radar_to_global_transform = tf_->lookupTransform(global_frame_, obstacle_array->header.frame_id, clock_->now(), rclcpp::Duration::from_seconds(2.0));
+      
+      tf2::Quaternion q(
+        radar_to_global_transform.transform.rotation.x,
+        radar_to_global_transform.transform.rotation.y,
+        radar_to_global_transform.transform.rotation.z,
+        radar_to_global_transform.transform.rotation.w);
+    
+      tf2::Matrix3x3 m(q);
+      double roll, pitch, yaw;
+      m.getRPY(roll, pitch, yaw);
+
+      RCLCPP_INFO(logger_, "roll: %f", roll);
+      RCLCPP_INFO(logger_, "pitch: %f", pitch);
+      RCLCPP_INFO(logger_, "yaw: %f", yaw);
+    }
 
 
 
