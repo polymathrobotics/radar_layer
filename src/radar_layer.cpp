8c06f9cce7c8f77edc77e586de1e66de9fb2a315
alexander <alex@polymathrobotics.com>
2024-05-10 09:45:08 -0700

    removed fake speeds, shortened time steps

--- src/radar_layer.cpp
+++ src/radar_layer.cpp
@@ -241,7 +241,7 @@ void RadarLayer::updateBounds(
       double min_probability = 0.05;
 
       int number_of_time_steps = 10.0;
-      double sample_time = 1.0;
+      double sample_time = 0.1;
 
       //Initial covariance scaling factor
       double sqrt_2_pi_det_covariance_0 = sqrt(2 * M_PI * obstacle_array->obstacles[i].position_covariance.x * obstacle_array->obstacles[i].position_covariance.y);
@@ -822,7 +822,7 @@ Eigen::VectorXd RadarLayer::projectMean(
 
   position(0) = obstacle.position.x;
   position(1) = obstacle.position.y;
-  velocity(0) = 1.0; //obstacle.velocity.x;
+  velocity(0) = obstacle.velocity.x;
   velocity(1) = obstacle.velocity.y;
 
   position_projected = position + time_steps * sample_time * velocity;
