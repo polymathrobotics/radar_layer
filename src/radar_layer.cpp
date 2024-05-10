0c7c90cc3d2f5b5f120d4e8019fb64c7dacbb695
alexander <alex@polymathrobotics.com>
2024-05-10 16:49:38 -0700

    lowered min probability

--- src/radar_layer.cpp
+++ src/radar_layer.cpp
@@ -33,7 +33,7 @@ void RadarLayer::onInitialize()
   declareParameter("enabled", rclcpp::ParameterValue(true));
   declareParameter("combination_method", rclcpp::ParameterValue(1));
   declareParameter("observation_sources",rclcpp::ParameterValue(std::string("")));
-  declareParameter("minimum_probability", rclcpp::ParameterValue(0.05));
+  declareParameter("minimum_probability", rclcpp::ParameterValue(0.01));
   declareParameter("number_of_time_steps", rclcpp::ParameterValue(10));
   declareParameter("sample_time",rclcpp::ParameterValue(0.1));
   declareParameter("stamp_footprint", rclcpp::ParameterValue(false));
